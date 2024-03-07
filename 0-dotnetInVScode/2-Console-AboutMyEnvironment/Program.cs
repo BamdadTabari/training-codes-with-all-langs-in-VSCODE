87fe19d4d2a090a0a99aac47d2aeb72870f92b6c
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-03-07 23:45:44 +0330

    notes and kosshers

--- 0-dotnetInVScode/2-Console-AboutMyEnvironment/Program.cs
+++ 0-dotnetInVScode/2-Console-AboutMyEnvironment/Program.cs
@@ -7,5 +7,6 @@ class Program
         Console.WriteLine(Environment.CurrentDirectory);
         Console.WriteLine(Environment.OSVersion.VersionString);
         Console.WriteLine("Namespace: {0}", typeof(Program).Namespace);
+        Console.WriteLine($"Computer named {Environment.MachineName} says \"No.\"");
     }
 }
